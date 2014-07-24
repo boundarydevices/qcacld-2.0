c37bf4cdbc9ec0081761c34ba3db44791878e5e1
Ming-yi Lin <mylin@qca.qualcomm.com>
2014-07-24 10:34:57 -0700

    Release 4.2.2.1
    Release 4.2.2.1 LNX.LEH
    
    Change-Id: Icb6dc6fa3db708c9fce564c2fecc917020ffbbf7

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -38,13 +38,13 @@ BRIEF DESCRIPTION:
 
 ===========================================================================*/
 
-#define QWLAN_VERSION_MAJOR            1
-#define QWLAN_VERSION_MINOR            0
-#define QWLAN_VERSION_PATCH            0
+#define QWLAN_VERSION_MAJOR            4
+#define QWLAN_VERSION_MINOR            2
+#define QWLAN_VERSION_PATCH            2
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            150
+#define QWLAN_VERSION_BUILD            1
 
-#define QWLAN_VERSIONSTR               "1.0.0.150"
+#define QWLAN_VERSIONSTR               "4.2.2.1"
 
 #ifdef QCA_WIFI_2_0
 
