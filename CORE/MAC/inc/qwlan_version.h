fd40eaae9a031ed25f2aae9f32d2f660ddcb1b4c
Ming-yi Lin <mylin@codeaurora.org>
2014-09-24 14:39:29 +0800

    Release 4.2.2.14
    Change-Id: I5d73e2ea3bce093cf855903899c437d357169e0f
    CRs-Fixed: 710831

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2 
 #define QWLAN_VERSION_PATCH            2
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            196
+#define QWLAN_VERSION_BUILD            14
 
-#define QWLAN_VERSIONSTR               "1.0.0.196"
+#define QWLAN_VERSIONSTR               "4.2.2.014"
 
 #ifdef QCA_WIFI_2_0
 
