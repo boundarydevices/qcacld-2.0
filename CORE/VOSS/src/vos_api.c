fd69032fb339b3e017ce52c9c926b7f9f4babff1
Edhar, Mahesh Kumar <c_medhar@qti.qualcomm.com>
2014-09-25 19:36:38 +0530

    QCACLD: SDIO:Swap mailbox of HTT Data and WMI control services.
    Change HTT data service to use mailbox0 which has window
    size of 56k and WMI control service to use mailbox1 which has
    window size of 36K.
    
    dynamically change the mapping of the HTT and WMI services to
    swap mailboxes based on firmware support for the same.
    
    Change-Id: I45e01bfbcfb189a2a8b194040a865fead203d018
    CRs-Fixed: 730152

--- CORE/VOSS/src/vos_api.c
+++ CORE/VOSS/src/vos_api.c
@@ -523,6 +523,7 @@ VOS_STATUS vos_open( v_CONTEXT_t *pVosContext, v_SIZE_t hddContextSize )
    }
 #endif
 
+   bmi_target_ready(scn);
    /* Open the SYS module */
    vStatus = sysOpen(gpVosContext);
 
