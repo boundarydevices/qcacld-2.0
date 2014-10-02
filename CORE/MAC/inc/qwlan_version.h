8f97a0fb5eb342c78c09bff59478b79a3a518b6c
Akash Patel <c_akashp@qca.qualcomm.com>
2014-10-02 09:09:47 -0700

    Release 1.0.0.204
    Release 1.0.0.204
    
    Change-Id: Ib9bfe83d009f53358fcafc05eb6c27b91b63ff1c
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            0 
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            203
+#define QWLAN_VERSION_BUILD            204
 
-#define QWLAN_VERSIONSTR               "1.0.0.203"
+#define QWLAN_VERSIONSTR               "1.0.0.204"
 
 #ifdef QCA_WIFI_2_0
 
