89d6e14ca8fa9dcfb1121c08e50f3721a4578970
syeh <syeh@qca.qualcomm.com>
2014-10-17 01:53:33 +0800

    qcacld: Fail to connect to AP (DIR-816L) with TKIP and hidden SSID
    Fail to connect to the AP (DIR-816L) which is configured with WPA/WPA2
    TKIP security and hidden SSID since the AP has no DSParam element in
    its probe response frame. Thus the channel number will be incorrect.
    
    Change-Id: I818a07d11c2b50ee18babc57f1cb9c6d70496d77
    CRs-Fixed: 740419

--- CORE/MAC/src/pe/lim/limApi.c
+++ CORE/MAC/src/pe/lim/limApi.c
@@ -1791,7 +1791,8 @@ limDetectChangeInApCapabilities(tpAniSirGlobal pMac,
             SIR_MAC_GET_SHORT_PREAMBLE(psessionEntry->limCurrentBssCaps) ) ||
           ( SIR_MAC_GET_QOS(apNewCaps.capabilityInfo) !=
             SIR_MAC_GET_QOS(psessionEntry->limCurrentBssCaps) ) ||
-          ( newChannel !=  psessionEntry->currentOperChannel )
+          ( (newChannel !=  psessionEntry->currentOperChannel) &&
+            (newChannel != 0) )
           ) ) )
     {
         if( false == psessionEntry->fWaitForProbeRsp )
