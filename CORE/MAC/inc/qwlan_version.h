6b1520c0c8e6bb15796c1c5764929061fbc7b782
siang <siang@codeaurora.org>
2014-11-18 14:52:02 +0800

    Rlease 4.2.2.27
    Change-Id: I3018c0220dbaf8f6f3ddf9032895073d9f648cfa
    CRs-Fixed: 710831

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            2
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            26
+#define QWLAN_VERSION_BUILD            27
 
-#define QWLAN_VERSIONSTR               "4.2.2.26"
+#define QWLAN_VERSIONSTR               "4.2.2.27"
 
 #ifdef QCA_WIFI_2_0
 
