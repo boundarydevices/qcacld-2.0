02957ab09bf5a7885da6c96d3049672946a89f44
siang <siang@codeaurora.org>
2014-12-18 10:33:22 +0800

    Release 4.2.2.36
    Change-Id: I1cfd20d971f09cc157828c8e5bb7d6431c0eabd0
    CRs-Fixed: 710831

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -39,12 +39,12 @@ BRIEF DESCRIPTION:
 ===========================================================================*/
 
 #define QWLAN_VERSION_MAJOR            4
-#define QWLAN_VERSION_MINOR            0
-#define QWLAN_VERSION_PATCH            10
+#define QWLAN_VERSION_MINOR            2
+#define QWLAN_VERSION_PATCH            2
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            001
+#define QWLAN_VERSION_BUILD            36
 
-#define QWLAN_VERSIONSTR               "4.0.10.001"
+#define QWLAN_VERSIONSTR               "4.2.2.36"
 
 #ifdef QCA_WIFI_2_0
 
