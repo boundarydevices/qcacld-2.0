a9d28b1358af0d7b356e7bc9a027ad9b5dd91836
Kai Liu <kaliu@codeaurora.org>
2015-03-30 13:47:51 +0800

    Release 4.5.10.013
    Change-Id: I593ebe31d940d666d5d826644d7d0bc6df274bea
    CRs-Fixed: 710831

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            5
 #define QWLAN_VERSION_PATCH            10
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            012
+#define QWLAN_VERSION_BUILD            013
 
-#define QWLAN_VERSIONSTR               "4.5.10.012"
+#define QWLAN_VERSIONSTR               "4.5.10.013"
 
 #ifdef QCA_WIFI_2_0
 
