7a0f88b192850baa9855d369102edb3f0706cfc0
Troy Kisky <troy.kisky@boundarydevices.com>
2020-05-19 17:21:32 -0700

    remove unused DMA_MAPPING_ERROR, defined differently in 5.4
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- CORE/SERVICES/HIF/sdio/linux/if_ath_sdio.h
+++ CORE/SERVICES/HIF/sdio/linux/if_ath_sdio.h
@@ -79,12 +79,6 @@ extern int pktlogmod_init(void *context);
 extern void pktlogmod_exit(void *context);
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,27)
-#define DMA_MAPPING_ERROR(dev, addr) dma_mapping_error((addr))
-#else
-#define DMA_MAPPING_ERROR(dev, addr) dma_mapping_error((dev), (addr))
-#endif
-
 int ath_sdio_probe(void *context, void *hif_handle);
 void ath_sdio_remove(void *context, void *hif_handle);
 int ath_sdio_suspend(void *context);
