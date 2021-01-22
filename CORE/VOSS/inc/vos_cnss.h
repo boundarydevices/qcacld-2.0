boundarydevices/qcacld-2.0

commit d0d9a539073562da817eb889eac7cb1d9ee11621

VOSS: update outdated APIs for kernel 5.4
Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

CORE/VOSS/inc/vos_cnss.h
@@ -38,6 +38,7 @@
 #include <linux/jiffies.h>
 #include <linux/workqueue.h>
 #include <linux/sched.h>
+#include <linux/version.h>
 
 enum cnss_bus_width_type {
 	CNSS_BUS_WIDTH_NONE,
@@ -87,7 +88,15 @@ static inline void vos_flush_delayed_work(void *dwork)
 static inline void vos_pm_wake_lock_init(struct wakeup_source *ws,
 					const char *name)
 {
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5,4,0))
 	wakeup_source_init(ws, name);
+#else
+	if (ws) {
+		memset(ws, 0, sizeof(*ws));
+		ws->name = name;
+	}
+	wakeup_source_add(ws);
+#endif
 }
 
 static inline void vos_pm_wake_lock(struct wakeup_source *ws)
@@ -108,7 +117,11 @@ static inline void vos_pm_wake_lock_release(struct wakeup_source *ws)
 
 static inline void vos_pm_wake_lock_destroy(struct wakeup_source *ws)
 {
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5,1,0))
 	wakeup_source_trash(ws);
+#else
+	wakeup_source_destroy(ws);
+#endif
 }
 
 static inline int vos_wlan_pm_control(bool vote)
@@ -120,7 +133,11 @@ static inline void vos_release_pm_sem(void) { return; }
 
 static inline void vos_get_monotonic_bootime_ts(struct timespec *ts)
 {
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4,20,0))
 	get_monotonic_boottime(ts);
+#else
+	*ts = ktime_to_timespec(ktime_get_boottime());
+#endif
 }
 
 static inline void vos_get_boottime_ts(struct timespec *ts)
@@ -198,7 +215,7 @@ static inline int vos_wlan_get_dfs_nol(void *info, u16 info_len)
 
 static inline void vos_get_monotonic_boottime_ts(struct timespec *ts)
 {
-	get_monotonic_boottime(ts);
+	vos_get_monotonic_bootime_ts(ts);
 }
 
 static inline void vos_schedule_recovery_work(struct device *dev) { return; }

CORE/VOSS/src/vos_api.c
@@ -3205,7 +3205,11 @@ v_U64_t vos_get_monotonic_boottime_ns(void)
 #if (LINUX_VERSION_CODE > KERNEL_VERSION(3, 10, 0))
 v_U64_t vos_get_bootbased_boottime_ns(void)
 {
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 3, 0))
 	return ktime_get_boot_ns();
+#else
+	return ktime_get_boottime_ns();
+#endif
 }
 
 #else

commit c3c14286a2ec55017fd1264bfd8322cd19b8cd67

VOSS: fix wakeup API call for 5.4 kernels
wakeup_source_trash() was actually doing a remove(), destroy is wrong
and can cause issues with suspend/resume.
See 623217a0cc45a ("PM / wakeup: Drop wakeup_source_drop()") for
details.

Fixes: d0d9a5390 ("VOSS: update outdated APIs for kernel 5.4")

Aligns with NXP modification in their LEA3.0 branch.

Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

CORE/VOSS/inc/vos_cnss.h
@@ -120,7 +120,8 @@ static inline void vos_pm_wake_lock_destroy(struct wakeup_source *ws)
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(5,1,0))
 	wakeup_source_trash(ws);
 #else
-	wakeup_source_destroy(ws);
+	wakeup_source_remove(ws);
+	__pm_relax(ws);
 #endif
 }
 